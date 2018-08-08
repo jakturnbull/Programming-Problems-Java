8012aeb05f899763be3e6fc609e24f799d44f300
jakturnbull <knightsdc2k12@gmail.com>
2018-08-08 10:47:30 +0100

    Loop to show factorials

---
+++ Problem1(factorial).java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+  public static int FirstFactorial (int num)
+  {
+  int factorial = 1;
+    for (int i = 1; i<=18; i++)
+      {
+          if (i==1)
+          {
+            int number= i*i;
+          }
+          else
+          {
+            int number1 = i*(i-1);
+            System
+          }
+      }
+
+
+
+    return num
+  }
+  public static void main(String[] args) {
+    Scanner s = new Scanner(Sysytem.in);
+    System.out.print(FirstFactorial(s.nextLine));
+  }
+}
